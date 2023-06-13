f162d41fb5dc3d0f895c51ec716435b6c0dcd2d2
Maksym Davydenko <maksydenko@gmail.com>
2023-06-13 13:27:53 +0300

    Minor fixes

--- src/components/base/Img/Img.tsx
+++ src/components/base/Img/Img.tsx
@@ -6,21 +6,28 @@ import { IImg } from "@/interfaces/img.interface";
 interface ImgProps {
   className: string;
   img: IImg;
+  style?: { [property: string]: string };
+  resetStyle?: boolean;
   width?: number;
   height?: number;
-  resetStyle?: boolean;
 }
 
 const Img: FC<ImgProps> = ({
   className,
   img: { src, alt },
+  style,
+  resetStyle = true,
   width = 0,
   height = 0,
-  resetStyle,
-}) => (
-  <div className={`${className}__img${resetStyle ? "" : " img"}`}>
-    <Image src={src} alt={alt} width={width} height={height} />
-  </div>
-);
+}) => {
+  return (
+    <div
+      className={`${className}__img${resetStyle ? " img" : ""}`}
+      style={style}
+    >
+      <Image src={src} alt={alt} width={width} height={height} />
+    </div>
+  );
+};
 
 export default Img;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => (
   <Html lang="en">
     <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-      />
       <link rel="apple-touch-icon" href="/voodoo/favicon.svg" />
       <link rel="icon" href="/voodoo/favicon.svg" />
-      <title></title>
     </Head>
     <body>
       <Main />
