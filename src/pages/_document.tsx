ea9ac6a78faf378652ca7ad31f6c02b5222a0dc5
Maksym Davydenko <maksydenko@gmail.com>
2023-06-26 20:24:35 +0300

    modified: _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => (
   <Html lang="en">
     <Head>
-      <link rel="apple-touch-icon" href="/voodoo/favicon.svg" />
-      <link rel="icon" href="/voodoo/favicon.svg" />
+      <link rel="apple-touch-icon" href="/favicon.svg" />
+      <link rel="icon" href="/favicon.svg" />
     </Head>
     <body>
       <Main />
